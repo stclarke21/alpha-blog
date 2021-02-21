69e432334233f3a1b44dad980245f1049c62746c
Argenis Morales <argbassist17@gmail.com>
2021-02-21 11:48:50 -0300

    Added create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,15 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        #render plain: @article.inspect
+        redirect_to article_path(@article)         
+    end
 end
