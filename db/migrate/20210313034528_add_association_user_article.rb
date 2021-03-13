9ecd7eaac6d9d9a392171803389a985c32c0dd9c
stclarke21 <argbassist17@gmail.com>
2021-03-13 01:14:20 -0300

    Updated db to mysql and added some article

---
+++ db/migrate/20210313034528_add_association_user_article.rb
@@ -0,0 +1,4 @@
+class AddAssociationUserArticle < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_010911) do
+ActiveRecord::Schema.define(version: 2021_03_13_034528) do
 
-  create_table "articles", force: :cascade do |t|
+  create_table "articles", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
