21b3e0f093be9140838b2b3a97ea7da8c0589a8d
stclarke21 <argbassist17@gmail.com>
2021-03-14 20:04:56 -0300

    Added functionality to edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,38 @@
 class UsersController < ApplicationController
-    
+    before_action :initialize_user, only: [:edit, :update, :show, :destory]
+
     def new
         @user = User.new
+    end
+
+    def edit
+
     end
 
     def create
         @user = User.new(users_params)
         if @user.save
-            flash[:notice] = "Welcome to the alpha blog #{@user.name} you have successfully signed up"         
+            flash[:notice] = "Welcome to the alpha blog #{@user.name} you have successfully signed up"
+            redirect_to articles_path         
         else
             render "new"
         end
     end
 
+    def update
+        if @user.update(users_params)
+            flash[:notice] = "The user #{@user.name} was updated successfully"
+            redirect_to articles_path        
+        else
+            render "edit"
+        end    
+    end
+
     private
+
+    def initialize_user
+        @user = User.find(params[:id])
+    end
 
     def users_params
         params.require(:user).permit(:name, :email, :password)
